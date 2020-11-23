822e656c6ce9fa36c59f7a8a5efc293a4897f72d
JuliaWolinowska <julia.wolinowska00@gmail.com>
2020-11-23 21:31:44 +0100

    d1

---
+++ POU.py
@@ -0,0 +1,7 @@
+def pou():
+    l = input('Podaj długość wahadła: ')
+    m = input('Wpisz masę wahadła:  ')
+    print('Ruch może odbywać się w jednym z podanych ośrodków: bla bla bla')
+    u = input('Napisz który z nich wybierasz: ')
+
+# opór, masa, długość,
