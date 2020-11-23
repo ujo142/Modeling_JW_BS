3493fb83955195b01d031d675a69cb2fe6939ad1
void <awekinderjoy@vp.pl>
2020-11-23 20:32:12 +0100

    d1

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print('hi')
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import sys
+import os
+from Powitanie import *
+from pozegnanie import *
+
+
+
+
+
+
+
+##############  Fragment do obsługi wyjątków ############################
+
+try:
+    powitanie()
+except IOError:                                             # Trzeba dopisywać na bierząco wyjątki
+    print('Błąd odczytu danych lub plik nie istnieje')      # Tutaj również trzeba będzie zmieniać na bierząco
+
+
+
+
+
+
+
+##############  Fragment do wyplucia danych o wahadłe  ###################
+
+
+
+
+
+##############  Fragment do wyświetlenia animacji / portretu fazowego ####
+
+
+
+
+
+##############  Fragment do siema / elo   ################################
+pozegnanie()
