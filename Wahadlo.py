ec04ac69a594c6508a182ef994537beb1a364046
JuliaWolinowska <julia.wolinowska00@gmail.com>
2020-11-27 22:00:19 +0100

    a

---
+++ Wahadlo.py
@@ -0,0 +1,9 @@
+# Matematyczny opis ruchu wahadła.
+
+
+# Kąty wychylenia w ruchu dla wahadła podwójnego
+# http://visual.icse.us.edu.pl/wizualizacje/mechanika-teoretyczna/zobacz/WahadloPodwojne/
+
+# Ruch wahadła pojedyńczego
+# https://pl.wikipedia.org/wiki/Wahad%C5%82o
+
